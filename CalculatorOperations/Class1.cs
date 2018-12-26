6cb08506e0ebcb8d1dff25764dc0329cc86de6a0
Divas Agarwal <dagarwal@tavisca.com>
2018-12-26 10:53:22 +0530

    initial commit

---
+++ CalculatorOperations/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorOperations
+{
+    /// <summary>
+    /// Defines set of mathematical operations
+    /// </summary>
+    public class Operation
+    {
+
+    }
+}
