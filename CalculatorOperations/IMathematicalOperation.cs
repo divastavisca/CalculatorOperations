b431e736dfa0e5aad53e3259da9db6de3f92f315
Divas Agarwal <dagarwal@tavisca.com>
2018-12-26 12:27:30 +0530

    added contracts and base implementation for exception handled operation

---
+++ CalculatorOperations/IMathematicalOperation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorOperations
+{
+    public interface IMathematicalOperation
+    {
+        int Add(int num1, int num2);
+
+        int Subtract(int num1, int num2);
+
+        int Multiply(int num1, int num2);
+
+        double Divide(int num1, int num2);
+    }
+}
--- CalculatorOperations/Operation.cs
+++ CalculatorOperations/Operation.cs
@@ -5,8 +5,26 @@ namespace CalculatorOperations
     /// <summary>
     /// Defines set of mathematical operations
     /// </summary>
-    public class Operation
+    public class ExceptionHandledOperation : IMathematicalOperation
     {
+        public int Add(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
 
+        public double Divide(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Subtract(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
